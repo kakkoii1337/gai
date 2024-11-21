250002b5271c62f0d3d8ef44e4c4401927bb463a
kakkoii1337 <kakkoii1337@gmail.com>
2024-11-21 18:09:16 +0800

    merge gai-mace node and user into single main.py

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -1,3 +1,14 @@
+# COPY TO VOLUME
+GAI_CONFIG="${HOME}/.gai/gai.yml"
+if [ ! -f ${GAI_CONFIG} ]; then
+    cp gai.yml ${GAI_CONFIG}
+fi
+
+GAI_PERSONA="${HOME}/.gai/persona/nodes"
+if [ ! -d ${GAI_PERSONA} ]; then
+    cp -rp gai-mace/src/gai/mace/node/persona/data $GAI_PERSONA
+fi
+
 python -m venv ~/.venv
 source ~/.venv/bin/activate
 pip install -e .
